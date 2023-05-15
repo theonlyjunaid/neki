70d60d71069d6bc5720a0b7529ecca0f6a27eaf3
junaid <onlyjunaid2002@gmail.com>
2023-05-15 13:09:34 +0530

    EFSD

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
               </div>
               <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                   <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                      Building a Wall of Goodness, Brick by Brick
+                      Giving is not just about making a donation, it's about making a difference
                       {/* <br className="hidden lg:inline-block" />
                       You can donate or ask for help */}
                   </h1>
